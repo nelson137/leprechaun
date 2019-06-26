ed40105c200a2c6557b3d512ebb8374ea0f7e9c0
Nelson Earle <nelson.earle137@gmail.com>
2019-06-26 12:24:10 -0500

    move helper methods into their own file

---
+++ OktaTest/Helpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace OktaTest
+{
+    public static class Helpers
+    {
+        private static readonly RNGCryptoServiceProvider _rng = new RNGCryptoServiceProvider();
+        private static readonly byte[] _randomBytes = new byte[4];
+
+        public static uint RandomUInt()
+        {
+            _rng.GetBytes(_randomBytes);
+            return BitConverter.ToUInt32(_randomBytes, 0);
+        }
+
+        public static char RandomAlphaNum()
+        {
+            var num = RandomUInt();
+            // Restrict the range to numbers and lowercase letters (36 total)
+            // Range starts at 48, the ascii code for '0'
+            num = (num % 36) + 48;
+            // If num <= 57 it's a number, else it's a letter (shift range up to start at lowercase letters)
+            return Convert.ToChar(num <= 57 ? num : num + 39);
+        }
+
+        public static string RandomString(int length)
+        {
+            string randomString = "";
+
+            for (var i = 0; i < length; i++)
+                randomString += RandomAlphaNum();
+
+            return randomString;
+        }
+
+        public static string ReadPassword(string prompt = "Password: ")
+        {
+            Console.Write(prompt);
+            var line = "";
+            ConsoleKeyInfo c;
+            while ((c = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+                line += c.KeyChar;
+            Console.Write(Environment.NewLine);
+            return line;
+        }
+    }
+}
--- OktaTest/Leprechaun.cs
+++ OktaTest/Leprechaun.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Net.Http;
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static OktaTest.Helpers;
 
 namespace OktaTest
 {
@@ -14,9 +14,6 @@ namespace OktaTest
         private const string _authServer = "ausly21wjd7rsff7G0x7";
         private const string _clientId = "0oaly1jsy9dS93SZQ0x7";
         private const string _redirectUri = "https://oidcdebugger.com/debug";
-
-        private static readonly RNGCryptoServiceProvider _rng = new RNGCryptoServiceProvider();
-        private static readonly byte[] _randomBytes = new byte[4];
 
         public static async Task<string> GetOktaAccessToken()
         {
@@ -38,7 +35,7 @@ namespace OktaTest
         private static async Task<string> GetOktaSessionToken()
         {
             var uri = "https://vu.okta.com/api/v1/authn";
-            var response = await new HttpClient().PostJsonAsync<AuthResponse>(uri, new
+            var response = await new HttpClient().PostJsonAsync<J_AuthResponse>(uri, new
             {
                 username = _username,
                 password = _password,
@@ -49,46 +46,6 @@ namespace OktaTest
                 }
             });
             return response.SessionToken;
-        }
-
-        public static string ReadPassword(string prompt = "Password: ")
-        {
-            Console.Write(prompt);
-            var line = "";
-            ConsoleKeyInfo c;
-            while ((c = Console.ReadKey(true)).Key != ConsoleKey.Enter)
-                line += c.KeyChar;
-            Console.Write(Environment.NewLine);
-            return line;
-        }
-
-        private static string RandomString(int length)
-        {
-            string randomString = "";
-
-            for (var i = 0; i < length; i++)
-                randomString += RandomAlphaNum();
-
-            return randomString;
-        }
-
-        private static char RandomAlphaNum()
-        {
-            var num = RandomUInt32();
-            // Restrict the range to numbers and lowercase letters (36 total)
-            // Range starts at 48, the ascii code for '0'
-            num = (num % 36) + 48;
-            // If num <= 57 it's a number, else it's a letter (shift range up to start at lowercase letters)
-            return Convert.ToChar(num <= 57 ? num : num + 39);
-        }
-
-        private static uint RandomUInt32()
-        {
-            using (var rng = _rng)
-            {
-                rng.GetBytes(_randomBytes);
-                return BitConverter.ToUInt32(_randomBytes, 0);
-            }
         }
     }
 }
